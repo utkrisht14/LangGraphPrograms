7e09efcfcd8227eca0744a04c4b5c134ecd99f2c
Utkrisht Mallick <utkrisht14@gmail.com>
2026-08-11 23:16:16 +0200

    graph file added.

---
+++ 3_Reflexion_Agent_Programs/ReflexionSupportAgentWithTools/graph.py
@@ -0,0 +1,61 @@
+from typing import Literal
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode
+
+from state import AgentState
+from nodes import generate_answer, review_answer, revise_answer
+from tools import search_company_policy, get_order_status
+
+MAX_ATTEMPTS = 3
+
+
+# ToolNode automatically executes tool calls made by the model.
+tool_node = ToolNode(
+    [
+        search_company_policy,
+        get_order_status
+    ]
+)
+
+
+def should_continue(state: AgentState) -> Literal["revise", "__end__"]:
+    """
+    Decide whether the answer should be revised again.
+    """
+
+    if state["approved"]:
+        return END
+
+    if state["iteration"] >= MAX_ATTEMPTS:
+        return END
+
+    return "revise"
+
+
+builder = StateGraph(AgentState)
+
+
+# Main Reflexion nodes.
+builder.add_node("generate", generate_answer)
+builder.add_node("review", review_answer)
+builder.add_node("revise", revise_answer)
+
+
+# Start with generation.
+builder.add_edge(START, "generate")
+builder.add_edge("generate", "review")
+
+builder.add_conditional_edges(
+    "review",
+    should_continue,
+    {
+        "revise": "revise",
+        END: END
+    }
+)
+
+# Review every revised answer again:
+builder.add_edge("revise", "review")
+
+graph = builder.compile()
